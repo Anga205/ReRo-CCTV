2f5cceef59c88a7facbf331389bff5c667f21089
Anga205 <github@angadbhalla.com>
2025-07-07 23:21:13 +0530

    MVP done

---
+++ frontend-vite/src/App.tsx
@@ -0,0 +1,11 @@
+import LiveStream from "./LiveStream";
+
+function App() {
+  return (
+    <div className="w-screen h-screen bg-amber-950">
+      <LiveStream />
+    </div>
+  );
+}
+
+export default App;
